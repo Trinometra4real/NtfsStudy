1610ae4dbbfad2d6651ab20ea2c851b1203ecfe2
Gourland Dorian <dorian.gourland@equasens.com>
2026-05-27 14:44:18 +0200

    Edited sh Script

--- create_disk.sh
+++ create_disk.sh
@@ -1,8 +1,8 @@
-PATH="$(pwd)/storage_disk.img"
-DEST="$(pwd)/VHD"
+VHD="$(pwd)/storage_disk.img"
+MOUNTP="$(pwd)/VHD"
 SIZE="2M"
 
-sudo dd if=/dev/zero of=$PATH/storage_disk.iso bs=$SIZE count=1  # bs = block size, count = number of blocks
-sudo mkfs -t ext4 $PATH/storage_disk.iso # sudo mkfs -t <file_system_type> <virtual hard disk>
-mkdir $DEST
-sudo mount -o loop $PATH/storage_disk.iso $DEST # mount virtual disk to target destination
+sudo dd if=/dev/zero of=$VHD/storage_disk.iso bs=$SIZE count=1  # bs = block size, count = number of blocks
+sudo mkfs -t ext4 $VHD/storage_disk.iso # sudo mkfs -t <file_system_type> <virtual hard disk>
+mkdir $MOUNTP
+sudo mount -o loop $VHD/storage_disk.iso $MOUNTP # mount virtual disk to target Destination
